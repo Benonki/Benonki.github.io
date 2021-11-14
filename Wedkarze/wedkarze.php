81195ec8613d915490338e29ec11e7a96fc142c3
Beniamin Żak <58690530+Benonki@users.noreply.github.com>
2021-11-14 22:47:21 +0100

    Update wedkarze.php

--- Wedkarze/wedkarze.php
+++ Wedkarze/wedkarze.php
@@ -13,9 +13,19 @@
 <img src = "szczupak.jpg" alt="szczupak">
 </div>
 <div id="p">
-  <?php
-  ?>
 <h3> Ryby spokojnego żeru (białe) </h3>
+<?php
+$connect = mysqli_connect("localhost","root","","wedkowanie1");
+$zapytanie = "SELECT id, nazwa, wystepowanie FROM ryby WHERE ryby.styl_zycia=2";
+$random= mysqli_query($connect,$zapytanie);
+while($linia = mysqli_fetch_row($random))
+{
+	echo $linia[0].'. '.$linia[1].' '.$linia[2];
+	echo '<br>';
+}
+mysqli_close($connect);
+?>
+<br>
 <br>
 <li>1.<a href = https://wedkuje.pl/ target ="_blank">„Odwiedź także”</a href></li>
 <li>1.<a href = http://www.pzw.org.pl/ target ="_blank">„Polski Związek Wędkarski”</a href></li>
