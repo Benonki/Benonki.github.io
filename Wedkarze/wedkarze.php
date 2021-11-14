22bd5d6cba6f5aeec1705f27189369b401982ee9
Beniamin Żak <58690530+Benonki@users.noreply.github.com>
2021-11-14 21:55:01 +0100

    Create wedkarze.php

---
+++ Wedkarze/wedkarze.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<title>Klub wędkowania</title>
+<link rel="stylesheet" href="styl2.css" type="text/css"/>
+<meta charset="UTF-8">
+</head>
+<body>
+<header>
+<h2>Wędkuj z nami</h2>
+</header>
+<div id="l">
+<img src = "szczupak.jpg" alt="szczupak">
+</div>
+<div id="p">
+<h3> Ryby spokojnego żeru (białe) </h3>
+<br>
+<li>1.<a href = https://wedkuje.pl/ target ="_blank">„Odwiedź także”</a href></li>
+<li>1.<a href = http://www.pzw.org.pl/ target ="_blank">„Polski Związek Wędkarski”</a href></li>
+</div>
+<footer>
+<p>„Stronę wykonał: Beniamin”</p>
+</footer>
+</body>
+</html>
