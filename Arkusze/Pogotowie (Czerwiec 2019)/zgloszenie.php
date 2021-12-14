a9fe37950437d54e3ec26f97a2061d39138d561c
Ben Ten <58690530+Benonki@users.noreply.github.com>
2021-12-14 16:38:24 +0100

    Add files via upload

---
+++ Arkusze/Pogotowie (Czerwiec 2019)/zgloszenie.php
@@ -0,0 +1,24 @@
+<!DOCTPE html>
+<html lang="pl">
+<head>
+<meta charset="utf-8"/>
+<TITLE>Pogotowie Ratunkowe</TITLE>
+</head>
+<body>
+<?php
+$n1=$_POST['n1'];
+$n2=$_POST['n2'];
+$a=$_POST['a'];
+$czas=date("H:i:sa");
+$connect=mysqli_connect("localhost", "root", "", "egzamin");
+if ($connect) {
+	$zapytanie="INSERT INTO zgloszenia VALUES (NULL, ".$n1.",".$n2.",".$a.",0,".$czas.")";
+	$result=mysqli_query($connect, $zapytanie);
+	mysqli_close($connect);
+}
+else {
+	echo "Nie mozna sie polaczyc z baza";
+}
+?>
+</body>
+</html>
