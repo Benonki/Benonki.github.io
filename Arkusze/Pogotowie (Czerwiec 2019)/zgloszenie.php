365dbc4bb81705a3cedc1eee0d30832e6ac1ca04
Ben Ten <58690530+Benonki@users.noreply.github.com>
2021-12-14 16:44:16 +0100

    Update zgloszenie.php

--- Arkusze/Pogotowie (Czerwiec 2019)/zgloszenie.php
+++ Arkusze/Pogotowie (Czerwiec 2019)/zgloszenie.php
@@ -12,7 +12,7 @@ $a=$_POST['a'];
 $czas=date("H:i:sa");
 $connect=mysqli_connect("localhost", "root", "", "egzamin");
 if ($connect) {
-	$zapytanie="INSERT INTO zgloszenia VALUES (NULL, ".$n1.",".$n2.",".$a.",0,".$czas.")";
+	$zapytanie="INSERT INTO zgloszenia VALUES (NULL, '.$n1.','.$n2.','.$a.',0,'.$czas.')";
 	$result=mysqli_query($connect, $zapytanie);
 	mysqli_close($connect);
 }
