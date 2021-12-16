3ee7a78f2b57670338afea25cdc9639cbe4e8b53
Ben Ten <58690530+Benonki@users.noreply.github.com>
2021-12-16 20:38:45 +0100

    Add files via upload

--- Arkusze/Wędkarze (Marzec 2019)/zgloszenie.php
+++ Arkusze/Wędkarze (Marzec 2019)/zgloszenie.php
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="utf-8"/>
+<TITLE>Wędkowanie - moje hobby</TITLE>
+</head>
+<body>
+<?php
+$num=$_POST['num'];
+$data=$_POST['dat'];
+$sed=$_POST['sed'];
+$connect=mysqli_connect("localhost","root","","egzamin");
+if($connect) {
+	$zapytanie = "Insert INTO zawody_wedkarskie VALUES (NULL,0,'.$num.','.$data.', '.$sed.')";
+	$wynik = mysqli_query($connect, $zapytanie);
+mysqli_close($connect);
+}
+else {
+	echo "Nie można połączyć się z bazą danych";	}
+?>
+</body>
+</html>
