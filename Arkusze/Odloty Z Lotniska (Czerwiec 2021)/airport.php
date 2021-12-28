5db9cb2c703e05c053c9a0a099f717f2274de77c
Ben Ten <58690530+Benonki@users.noreply.github.com>
2021-12-28 21:51:54 +0100

    Update airport.php

--- Arkusze/Odloty Z Lotniska (Czerwiec 2021)/airport.php
+++ Arkusze/Odloty Z Lotniska (Czerwiec 2021)/airport.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
-<html lang = "pl" >
+<html lang="pl">
 <head>
-<meta charset = "utf-8" />
-<title> Odloty samolotów </title>
-<link rel = "stylesheet" href = "styl6.css" />
+<meta charset="utf-8"/>
+<title> Odloty samolotów</title>
+<link rel="stylesheet" href="styl6.css"/>
 </head>
 <body>
-<div id = "Baner1" >
-<h2> Odloty z lotniska </h2>
+<div id="b1">
+<h2> Odloty z lotniska</h2>
 </div>
-<div id = "Baner2" >
-<img src = "zad6.png" alt = "logotyp" > 
+<div id="b2">
+<img src="zad6.png" alt="logotyp"> 
 </div>
 <main>
-<h4> Tabela odlotów </h4> 
+<h4>Tabela odlotów</h4> 
 <table>
 <tr>
 <td> lp </td> <td> numer rejsu </td> <td> czas </td> <td> kierunek </td> <td> status </td> 
 </tr>
 <?php
-$connect = mysqli_connect("localhost", "root", "", "egzamin3");
+$connect=mysqli_connect("localhost", "root", "", "egzamin3");
 if ($connect) {
 	$zapytanie = "SELECT id, nr_rejsu, czas, kierunek, status_lotu FROM odloty ORDER BY czas desc";
 	$wynik = mysqli_query($connect, $zapytanie);
-	while ($linia = mysqli_fetch_array($wynik)) {
+	while ($linia=mysqli_fetch_array($wynik)) {
 		echo '<tr>';
 		echo '<td>'.$linia['id'].'</td>';
 		echo '<td>'.$linia['nr_rejsu'].'</td>';
@@ -32,21 +32,17 @@ if ($connect) {
 		echo '<td>'.$linia['status_lotu'].'</td>';
 		echo '</tr>';
 	}
-
-
-mysqli_close($connect);	
-}
-
+mysqli_close($connect);	}
 ?>
 </table>
 </main>
-<div id = "Stopka1" >
-<a href = "kwerendy.txt" > Pobierz obraz </a>
+<div id="s1">
+<a href="kwerendy.txt">Pobierz obraz</a>
 </div>
-<div id = "Stopka2" >
+<div id="s2">
 <?php
-$czas = 3600; 
-setcookie ("UserVisit", $czas);
+$czas=3600; 
+setcookie("UserVisit", $czas);
 if (isSet ($_COOKIE ['UserVisit']))  { 
 echo '<p>'."Miło nam, że nas znowu odwiedziłeś".'</p>'; 
 }  else  { 
@@ -54,8 +50,8 @@ echo '<p>'.'<i>'."Dzień Dobry! Sprawdź regulamin naszej strony".'</i>'.'</p>';
 }
 ?>
 </div>
-<div id = "Stopka3" >
-Autor: BGBŻ
+<div id="s3">
+Autor: Beniamin
 </div>
 </body>
 </html>
